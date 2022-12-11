cb7dd9029ae06444894e8b87e5d7a2a49815dde7
AndradeCCS <89852118+AndradeCCS@users.noreply.github.com>
2022-12-11 16:35:53 -0300

    testando commit
    testando  commit

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -26,16 +26,11 @@ public class Topico {
 
 	public Topico() {
 	}
-
-
 	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
 		this.mensagem = mensagem;
 		this.curso = curso;
 	}
-
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -43,7 +38,6 @@ public class Topico {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
